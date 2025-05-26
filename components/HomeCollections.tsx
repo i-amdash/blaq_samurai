602ca552670c445698f07c24b576c847d0c32653
i-amdash <ashimidoyin@yahoo.com>
2025-05-26 03:14:12 +0100

    added footer

--- components/HomeCollections.tsx
+++ components/HomeCollections.tsx
@@ -32,7 +32,7 @@ const HomeCollections = () => {
           />
           <div className="absolute inset-0 bg-black opacity-50" />
           <div className="relative z-10 text-center">
-            <h3 className="text-3xl font-bold uppercase mb-4">Custom Orders</h3>
+            <h3 className="xl:text-3xl text-xl font-bold uppercase mb-4">Custom Orders</h3>
             <div className="flex flex-row justify-center items-center">
               <div className="border border-1 rounded-full px-4 py-2 cursor-pointer hover:bg-primary hover:text-white hover:border-none transition-colors ">
                 <p className="font-light text-sm">Discover more</p>
@@ -49,7 +49,7 @@ const HomeCollections = () => {
           />
           <div className="absolute inset-0 bg-black opacity-50" />
           <div className="relative z-10 text-center">
-            <h3 className="text-3xl font-bold uppercase mb-4">The Orange Collection</h3>
+            <h3 className="xl:text-3xl text-xl font-bold uppercase mb-4">The Orange Collection</h3>
             <div className="flex flex-row justify-center items-center">
               <div className="border border-1 rounded-full px-4 py-2 cursor-pointer hover:bg-primary hover:text-white hover:border-none transition-colors ">
                 <p className="font-light text-sm">Discover more</p>
@@ -66,7 +66,7 @@ const HomeCollections = () => {
           />
           <div className="absolute inset-0 bg-black opacity-50" />
           <div className="relative z-10 text-center">
-            <h3 className="text-3xl font-bold uppercase mb-4">Featured Collections</h3>
+            <h3 className="xl:text-3xl text-xl font-bold uppercase mb-4">Featured Collections</h3>
             <div className="flex flex-row justify-center items-center">
               <div className="border border-1 rounded-full px-4 py-2 cursor-pointer hover:bg-primary hover:text-white hover:border-none transition-colors ">
                 <p className="font-light text-sm">Discover more</p>
@@ -83,7 +83,7 @@ const HomeCollections = () => {
           />
           <div className="absolute inset-0 bg-black opacity-50" />
           <div className="relative z-10 text-center">
-            <h3 className="text-3xl font-bold uppercase mb-4">Totem Automobili</h3>
+            <h3 className="xl:text-3xl text-xl font-bold uppercase mb-4">Totem Automobili</h3>
             <div className="flex flex-row justify-center items-center">
               <div className="border border-1 rounded-full px-4 py-2 cursor-pointer hover:bg-primary hover:text-white hover:border-none transition-colors ">
                 <p className="font-light text-sm">Discover more</p>
